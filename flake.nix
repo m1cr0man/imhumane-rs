0a111fe0c0d6a1f815b0c66ec6f57cec0e969847
Lucas Savva <lucas@m1cr0man.com>
2023-12-02 21:43:20 +0000

    Update project framework

--- flake.nix
+++ flake.nix
@@ -1,43 +1,169 @@
 {
-  description = "I'm Humane human validator";
+  description = "Build a cargo project";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      # inputs.rust-analyzer-src.follows = "";
+    };
+
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, crane, fenix, flake-utils, advisory-db, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
-          inherit system overlays;
+          inherit system;
         };
-        devDependencies = with pkgs; [
-          openssl
-          pkgconfig
-          (rust-bin.nightly."2023-02-17".default.override {
-            extensions = [ "rust-src" "rustfmt" ];
-          })
-          lldb
+
+        stdenv =
+          if pkgs.stdenv.isLinux then
+            pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv
+          else
+            pkgs.stdenv;
+
+        inherit (pkgs) lib;
+
+        craneLib = crane.mkLib pkgs;
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+        mkToolchain = fenix.packages.${system}.combine;
+
+        toolchain = fenix.packages.${system}.stable;
+
+        buildToolchain = mkToolchain (with toolchain; [
+          cargo
+          rustc
+        ]);
+
+        craneLibBuild = craneLib.overrideToolchain buildToolchain;
+
+        devToolchain = mkToolchain (with toolchain; [
+          cargo
+          clippy
+          rust-src
+          rustc
+          llvm-tools
           rust-analyzer
-          cmake
-          libev
-          uthash
-        ];
+
+          # Always use nightly rustfmt because most of its options are unstable
+          fenix.packages.${system}.latest.rustfmt
+        ]);
+
+        craneLibDev = craneLib.overrideToolchain devToolchain;
+
+        # Common arguments can be set here to avoid repeating them later
+        commonArgs = {
+          inherit src stdenv;
+          strictDeps = true;
+
+          buildInputs = [
+            # Add additional build inputs here
+          ] ++ lib.optionals pkgs.stdenv.isDarwin [
+            # Additional darwin specific inputs can be set here
+            pkgs.libiconv
+          ];
+
+          # Additional environment variables can be set directly
+          # MY_CUSTOM_VAR = "some value";
+        };
+
+        # Build *just* the cargo dependencies, so we can reuse
+        # all of that work (e.g. via cachix) when running in CI
+        cargoArtifacts = craneLibBuild.buildDepsOnly commonArgs;
+
+        # Build the actual crate itself, reusing the dependency
+        # artifacts from above.
+        imhumane-rs = craneLibBuild.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+        });
       in
-      rec {
-        devShells.default = pkgs.mkShell {
-          buildInputs = devDependencies;
+      {
+        checks = {
+          # Build the crate as part of `nix flake check` for convenience
+          inherit imhumane-rs;
+
+          # Run clippy (and deny all warnings) on the crate source,
+          # again, resuing the dependency artifacts from above.
+          #
+          # Note that this is done as a separate derivation so that
+          # we can block the CI if there are issues here, but not
+          # prevent downstream consumers from building our crate by itself.
+          imhumane-rs-clippy = craneLibDev.cargoClippy (commonArgs // {
+            inherit cargoArtifacts;
+            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+          });
+
+          imhumane-rs-doc = craneLibDev.cargoDoc (commonArgs // {
+            inherit cargoArtifacts;
+          });
+
+          # Check formatting
+          imhumane-rs-fmt = craneLibDev.cargoFmt {
+            inherit src;
+          };
+
+          # Audit dependencies
+          imhumane-rs-audit = craneLibDev.cargoAudit {
+            inherit src advisory-db;
+          };
+
+          # Audit licenses
+          imhumane-rs-deny = craneLibDev.cargoDeny {
+            inherit src;
+          };
+
+          # Run tests with cargo-nextest
+          # Consider setting `doCheck = false` on `imhumane-rs` if you do not want
+          # the tests to run twice
+          imhumane-rs-nextest = craneLibDev.cargoNextest (commonArgs // {
+            inherit cargoArtifacts;
+            partitions = 1;
+            partitionType = "count";
+          });
         };
 
-        packages.venv = pkgs.symlinkJoin {
-          name = "imhumane-rs-venv";
-          paths = devDependencies;
+        packages = {
+          default = imhumane-rs;
+          imhumane-rs-llvm-coverage = craneLibDev.cargoLlvmCov (commonArgs // {
+            inherit cargoArtifacts;
+          });
+          devTools = pkgs.linkFarm "vscode-dev-tools" {
+            inherit (pkgs) nixpkgs-fmt rnix-lsp gcc pkg-config;
+            rust = devToolchain;
+          };
         };
 
-        devShell = devShells.default;
+        apps.default = flake-utils.lib.mkApp {
+          drv = imhumane-rs;
+        };
+
+        devShells.default = craneLibDev.devShell {
+          # Inherit inputs from checks.
+          checks = self.checks.${system};
+
+          # Additional dev-shell environment variables can be set directly
+          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+          RUST_SRC_PATH = "${devToolchain}/lib/rustlib/src/rust/library";
+
+          # Extra inputs can be added here; cargo and rustc are provided by default.
+          packages = [
+            # pkgs.ripgrep
+          ];
+        };
       }
     );
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,2 @@
-pub mod service;
 pub mod http;
+pub mod service;
